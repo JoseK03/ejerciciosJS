3c291f3d27803bacb6f465a5d59b8dfcf24c3893
JoseK03 <dajoseva@gmail.com>
2023-08-30 17:18:21 -0500

    feat: :construction: ejercicio por terminar fecha navidad

---
+++ NocheBuena/main.js
@@ -0,0 +1,8 @@
+/* La Nochebuena ya casi está aquí, así que, naturalmente, ¡necesitamos preparar un poco de leche y galletas para Papá Noel! Cree una función que acepte un objeto Fecha y devuelva verdadero si es Nochebuena (24 de diciembre) y falso en caso contrario. Tenga en cuenta que la fecha del mes de JavaScript está basada en 0, lo que significa que diciembre es el mes número 11, mientras que enero es 0. */
+
+//!----------------------------------------------------------------------
+
+const fecha = document.getElementById('fecha');
+const fechaValor = fecha.value;
+
+let fechaa = prompt()
